4ca439448b2e917e85c8223d685f705b810e8b60
Joshua Jackson <joshua.j@mmtdigital.co.uk>
2018-07-02 14:02:42 +0100

    Expanded more on middleware

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ let uk = require('./uk');
 
 let france = require('./france');
 
+
+/* 
+  *                                   -------- MIDDLEWARE --------
+
+  ! app.get, app.use, app.post etc etc can all feature some sort of middleware inside of the callback function
+  ! that gets invoked. They can either just serve a resource, or carry out an action and then call 'next()' to
+  ! move on to the next piece of middleware, or they can end the response lifecycle 'res.end()'.
+*/
+
+const currentDateMiddleware = function (req, res, next)
+{
+  console.log(Date.now());
+  next();
+}
+
+//First middleware function.
+app.use((req, res, next) =>
+{
+  console.log("First middleware function.");
+  console.log(req);
+  next();
+});
+
+//Second middleware function.
+app.use(currentDateMiddleware);
+
+//Last middleware function.
+app.use((req, res, next) =>
+{
+  console.log("Last middleware function.");
+  res.end();
+});
+
+
 // If the route is /uk, then go to the uk.js routing and let that decide what gets sent (ie. the "Hello Uk!" message probably)
 app.use('/uk', uk);
 
@@ -18,7 +52,6 @@ app.get('/', (req, res) =>
 {
   res.send("Hello World!");
 });
-
 
 app.listen(3000, () =>
 {
